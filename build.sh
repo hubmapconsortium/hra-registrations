3f2d4ef85f3e7cd276bf512a8ef4b745b77bdfe1
Bruce W. Herr II <bherr@indiana.edu>
2023-08-09 01:54:48 -0400

    Update to add / fix thumbnails

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 for f in */registrations.yaml */*/registrations.yaml; do
-  npx github:hubmapconsortium/hra-rui-locations-processor normalize `dirname $f`
+  if [ "$1" == "local" ]; then
+    node ../hra-rui-locations-processor/src/cli.js normalize `dirname $f`
+  else
+    npx github:hubmapconsortium/hra-rui-locations-processor normalize `dirname $f`
+  fi
 done
